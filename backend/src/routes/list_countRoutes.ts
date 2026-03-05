0d61b6276e10bb06d60f658728657137c8aadef2
MuriloDalMolim <murilodalmolim@gmail.com>
2026-03-05 20:35:02 -0300

    fix(list_count): add ":" to the getCountById endpoint

--- backend/src/routes/list_countRoutes.ts
+++ backend/src/routes/list_countRoutes.ts
@@ -4,7 +4,7 @@ import { listCountController } from "../controller/list_countController.js";
 
 const router = express.Router()
 
-router.get("/listCountId", authMiddleware, listCountController.getCountById)
+router.get("/:listCountId", authMiddleware, listCountController.getCountById)
 router.get("/", authMiddleware, listCountController.getAllCounts)
 router.post("/:listId", authMiddleware, listCountController.startCount)
 router.patch("/:listCountId/close", authMiddleware, listCountController.closeCount)
