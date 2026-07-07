04f1ceb21d949eed9654fce989a26abcf9236958
MuriloDalMolim <murilodalmolim@gmail.com>
2026-07-07 19:38:53 -0300

    deploy settings

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,7 +4,7 @@ import { Pool } from 'pg';
 
 const connectionString = process.env.DATABASE_URL!;
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
